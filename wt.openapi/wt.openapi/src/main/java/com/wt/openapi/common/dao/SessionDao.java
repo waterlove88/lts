26618f9ca79d4b7cac54bb7058ddf7aadbeeebca
waterlove88 <waterlove88@hellonature.net>
2018-10-15 18:40:41 +0900

    refesh project

--- wt.openapi/wt.openapi/src/main/java/com/wt/openapi/common/dao/SessionDao.java
+++
@@ -1,9 +0,0 @@
- package com.wt.openapi.common.dao;
-
-import com.wt.openapi.common.model.RequestCustomHeaderInfo;
-
-public interface SessionDao {
-	RequestCustomHeaderInfo getUserInfoBySession(String sessionId);
-	int deleteSessionId(String sessionId);
-	int insertSessionId(RequestCustomHeaderInfo sessionInfo);
-}
